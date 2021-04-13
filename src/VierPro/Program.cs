a6e27c8db8439489b88cc82a7a75ea18d5a6d608
Felix Damrau <mail@felixdamrau.de>
2021-04-13 18:37:34 +0200

    Remove forgotten BlazorTable stuff

--- src/VierPro/Program.cs
+++ src/VierPro/Program.cs
@@ -1,7 +1,6 @@
 using System;
 using System.Net.Http;
 using System.Threading.Tasks;
-using BlazorTable;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using MudBlazor.Services;
@@ -16,7 +15,6 @@ namespace VierPro
             builder.RootComponents.Add<App>("#app");
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-            builder.Services.AddBlazorTable();
             builder.Services.AddMudServices();
             await builder.Build().RunAsync();
         }
