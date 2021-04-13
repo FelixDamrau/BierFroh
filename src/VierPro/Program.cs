93db2511fa8ad6f4b5b58a5b713428d6992d2c59
Felix Damrau <felixdamrau@gmail.com>
2021-04-13 18:43:17 +0200

    Use MudBlazor / Material Design / remove bootstrap
    Co-authored-by: Felix Damrau <mail@felixdamrau.de>

--- src/VierPro/Program.cs
+++ src/VierPro/Program.cs
@@ -1,9 +1,9 @@
 using System;
 using System.Net.Http;
 using System.Threading.Tasks;
-using BlazorTable;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.DependencyInjection;
+using MudBlazor.Services;
 
 namespace VierPro
 {
@@ -15,7 +15,7 @@ namespace VierPro
             builder.RootComponents.Add<App>("#app");
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-            builder.Services.AddBlazorTable();
+            builder.Services.AddMudServices();
             await builder.Build().RunAsync();
         }
     }
