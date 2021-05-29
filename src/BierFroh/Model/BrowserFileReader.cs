8dc3ce437a9ab3a19e9cf6aea8f2b0e23dd09ffd
Felix Damrau <mail@felixdamrau.de>
2021-05-30 00:10:31 +0200

    Clean-up a tiny bit - extract parse logic

---
+++ src/BierFroh/Model/BrowserFileReader.cs
@@ -0,0 +1,22 @@
+﻿using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components.Forms;
+
+namespace BierFroh.Model
+{
+    public static class BrowserFileReader
+    {
+        public static async Task<string> ReadFile(IBrowserFile browserFile)
+        {
+            using var stream = browserFile.OpenReadStream();
+            using var reader = new StreamReader(stream);
+            var stringBuilder = new StringBuilder();
+            while (await reader.ReadLineAsync() is { } line)
+            {
+                stringBuilder.AppendLine(line);
+            }
+            return stringBuilder.ToString();
+        }
+    }
+}
---
+++ src/BierFroh/Model/ChartSeriesHelper.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using KickTippHistory.Core.Model;
+using MudBlazor;
+
+namespace BierFroh.Model
+{
+    public class ChartSeriesHelper
+    {
+        public static IEnumerable<ChartSeries> Convert(Season season)
+        {
+            foreach (var result in season.PlayerResults)
+            {
+                var data = Enumerable.Range(1, 34)
+                    .Select(i => result.GetMatchDayPoints(i))
+                    .Select(r => r.Valid ? (double)r.Value : 0)
+                    .ToArray();
+
+                for (var i = 1; i < data.Length; ++i)
+                {
+                    data[i] += data[i - 1];
+                }
+                yield return new ChartSeries { Name = result.PlayerName, Data = data };
+            }
+        }
+
+        public static IEnumerable<ChartSeries> CleanUp(IEnumerable<ChartSeries> chartSeries)
+        {
+            foreach (var serie in chartSeries)
+            {
+                if (serie.Data.Any(d => d != 0))
+                    yield return serie;
+            }
+        }
+    }
+}
