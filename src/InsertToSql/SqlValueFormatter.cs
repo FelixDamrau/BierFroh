1734cc381eec30c18f7066e4004c23d2f49d90bc
Felix Damrau <felixdamrau@gmail.com>
2021-06-03 13:30:34 +0200

    Add InsertToSql page (#12)
    Parses a sql query result to a INSERT statement
    Allow editing of raw query data
    Parse numbers and DbNULL as such and everything else as sql strings
    
    Co-authored-by: Felix Damrau <mail@felixdamrau.de>

---
+++ src/InsertToSql/SqlValueFormatter.cs
@@ -0,0 +1,27 @@
+﻿using System.Globalization;
+
+namespace InsertToSql
+{
+    public static class SqlValueFormatter
+    {
+        public static string Parse(string value)
+        {
+            return value switch
+            {
+                "" => "''",
+                "NULL" => "NULL",
+                _ when IsNumeric(value) => value,
+                _ => $"'{value}'"
+            };
+        }
+
+        private static bool IsNumeric(string value)
+        {
+            return double.TryParse(
+                value,
+                NumberStyles.AllowLeadingSign | NumberStyles.AllowDecimalPoint,
+                CultureInfo.InvariantCulture,
+                out var _);
+        }
+    }
+}
---
+++ tests/InsertToSql.Tests/SqlValueFormatterTests.cs
@@ -0,0 +1,52 @@
+﻿using Xunit;
+
+namespace InsertToSql.Tests
+{
+    public class SqlValueFormatterTests
+    {
+        [Fact]
+        public void EmptyInputReturnsEmptySqlString()
+        {
+            var result = SqlValueFormatter.Parse(string.Empty);
+
+            Assert.Equal("''", result);
+        }
+
+        [Theory]
+        [InlineData("1")]
+        [InlineData("-1")]
+        [InlineData("1234")]
+        [InlineData("123.456")]
+        [InlineData("-123.567")]
+        public void NumericInputInvariantReturn(string numericInputValue)
+        {
+            var result = SqlValueFormatter.Parse(numericInputValue);
+
+            Assert.Equal(numericInputValue, result);
+        }
+
+        [Theory]
+        [InlineData("2031-12-12 00:00:00.000")]
+        [InlineData("2031-12-12")]
+        [InlineData("2031-12")]
+        [InlineData("Foo")]
+        [InlineData("F08F3672-2AEF-4051-8ABB-469B59AE0249")]
+        public void OtherInputReturnsSqlString(string otherInputValue)
+        {
+            var result = SqlValueFormatter.Parse(otherInputValue);
+
+            Assert.Equal($"'{otherInputValue}'", result);
+        }
+
+        [Fact]
+        public void NullStringInputInvariantReturn()
+        {
+            const string nullString = "NULL";
+
+            var result = SqlValueFormatter.Parse(nullString);
+
+            Assert.Equal(nullString, result);
+        }
+    }
+}
+
