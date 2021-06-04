d230ddcf37d138e2440b6538e7fbae4cdaec8a31
Felix Damrau <damrau@fastec.de>
2021-06-04 07:03:44 +0200

    Format numbers as string for SQL queries
    * let SQL server parse the values
    * enforces US number notation

--- tests/InsertToSql.Tests/SqlValueFormatterTests.cs
+++ tests/InsertToSql.Tests/SqlValueFormatterTests.cs
@@ -4,34 +4,19 @@ namespace InsertToSql.Tests
 {
     public class SqlValueFormatterTests
     {
-        [Fact]
-        public void EmptyInputReturnsEmptySqlString()
-        {
-            var result = SqlValueFormatter.Parse(string.Empty);
-
-            Assert.Equal("''", result);
-        }
-
         [Theory]
-        [InlineData("1")]
+        [InlineData("")]
         [InlineData("-1")]
+        [InlineData("1")]
         [InlineData("1234")]
         [InlineData("123.456")]
         [InlineData("-123.567")]
-        public void NumericInputInvariantReturn(string numericInputValue)
-        {
-            var result = SqlValueFormatter.Parse(numericInputValue);
-
-            Assert.Equal(numericInputValue, result);
-        }
-
-        [Theory]
         [InlineData("2031-12-12 00:00:00.000")]
         [InlineData("2031-12-12")]
         [InlineData("2031-12")]
         [InlineData("Foo")]
         [InlineData("F08F3672-2AEF-4051-8ABB-469B59AE0249")]
-        public void OtherInputReturnsSqlString(string otherInputValue)
+        public void AnyNotNullInputReturnsSqlString(string otherInputValue)
         {
             var result = SqlValueFormatter.Parse(otherInputValue);
 
