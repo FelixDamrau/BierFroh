53145c32e2fda9a12e12700fed7c475568bebbe3
Felix Damrau <mail@felixdamrau.de>
2021-06-06 12:53:04 +0200

    Add autocomplete chooser and use browser cache

---
+++ src/BierFroh/Model/BrowserStorage/AppState.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BierFroh.Model.BrowserStorage
+{
+    internal record AppState(DateTime LastVisit, InsertToSqlState InsertToSqlState)
+    {
+        public static AppState Default = new(
+            DateTime.Now,
+            new InsertToSqlState(Array.Empty<string>()));
+    }
+
+    internal record InsertToSqlState(IEnumerable<string> CachedTableNames);
+}
--- src/BierFroh/Program.cs
+++ src/BierFroh/Program.cs
@@ -1,8 +1,10 @@
 using System;
 using System.Net.Http;
 using System.Threading.Tasks;
+using BierFroh.Model.BrowserStorage;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.JSInterop;
 using MudBlazor.Services;
 
 namespace BierFroh
@@ -15,6 +17,7 @@ namespace BierFroh
             builder.RootComponents.Add<App>("#app");
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            builder.Services.AddSingleton(sp => new BrowserStorageProvider(sp.GetRequiredService<IJSRuntime>()));
             builder.Services.AddMudServices();
             await builder.Build().RunAsync();
         }
