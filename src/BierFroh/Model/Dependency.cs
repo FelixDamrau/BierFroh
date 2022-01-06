4c0e8e43fe971ee6bd8025082e2c2be1c0f661c6
Felix Damrau <mail@felixdamrau.de>
2022-01-06 15:44:40 +0100

    Add custom graph node component

---
+++ src/BierFroh/Model/Dependency.cs
@@ -0,0 +1,11 @@
+﻿using Blazor.Diagrams.Core.Geometry;
+using Blazor.Diagrams.Core.Models;
+
+namespace BierFroh.Model;
+public class Dependency : NodeModel
+{
+    public Dependency(Point point)
+        : base(point)
+    {
+    }
+}
