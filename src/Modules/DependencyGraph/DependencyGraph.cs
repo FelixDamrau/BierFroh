e8ab79d240ff172656388255aec416e2545d6b21
Felix Damrau <mail@felixdamrau.de>
2022-01-09 21:45:46 +0100

    Filter graph for the first framework found

--- src/Modules/DependencyGraph/DependencyGraph.cs
+++ src/Modules/DependencyGraph/DependencyGraph.cs
@@ -5,10 +5,10 @@ namespace BierFroh.Modules.DependencyGraph;
 
 public class DependencyGraph
 {
-    public static IDirectedGraph<GraphNode> Create(IProjectAssets projectAssets)
+    public static IDirectedGraph<GraphNode> Create(IProjectAssets projectAssets, string framework)
     {
         var graphBuilder = GraphBuilder<GraphNode>.Create();
-        foreach (var dependency in projectAssets.Dependencies)
+        foreach (var dependency in projectAssets.Dependencies.Where(d => d.Framework == framework))
         {
             graphBuilder = graphBuilder.AddVertex(new GraphNode(dependency.Name, dependency.Framework, dependency.Version))
                 .WithSuccessors(dependency.Dependencies.Select(d => new GraphNode(d.Name, d.Framework, d.Version)));
