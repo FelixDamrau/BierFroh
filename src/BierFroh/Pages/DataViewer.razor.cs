76eea95bc9aba4fcbe327ecba83004ff7f07355a
Felix Damrau <mail@felixdamrau.de>
2024-05-12 18:43:49 +0200

    Allow pre-parsing, disable buttons when parsing
    (pls dont look at the async code...)

--- src/BierFroh/Pages/DataViewer.razor.cs
+++ src/BierFroh/Pages/DataViewer.razor.cs
@@ -5,31 +5,49 @@ using DevLab.JmesPath;
 using Microsoft.AspNetCore.Components.Forms;
 using Microsoft.AspNetCore.Components.Web;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace BierFroh.Pages;
 public partial class DataViewer
 {
     private const long maxFileSize = 209_715_200; // 200MB
     private string? xmlText;
-    private string? jsonText;
+    private string? xmlErrorText;
+
+    private string jsonText = string.Empty;
+    private JToken? jToken;
+    private string? jsonPreParseError;
+
     private string? query;
     private string queryResult = string.Empty;
     private string cleanResults = string.Empty;
 
     private bool parsing = false;
+    private bool preParsing = false;
 
-    private void Parse()
+    private void ParseXml()
     {
+        xmlErrorText = null;
         if (string.IsNullOrEmpty(xmlText))
         {
             jsonText = string.Empty;
             return;
         }
-        var xmlDocument = new XmlDocument();
-        xmlDocument.LoadXml(xmlText);
-        RemoveIrrelevantXmlNodes(xmlDocument);
-
-        jsonText = JsonConvert.SerializeXmlNode(xmlDocument, Newtonsoft.Json.Formatting.Indented, omitRootObject: true);
+        try
+        {
+            var xmlDocument = new XmlDocument();
+            xmlDocument.LoadXml(xmlText);
+            RemoveIrrelevantXmlNodes(xmlDocument);
+            jsonText = JsonConvert.SerializeXmlNode(xmlDocument, Newtonsoft.Json.Formatting.Indented, omitRootObject: true);
+        }
+        catch (Exception ex)
+        {
+            jsonText = string.Empty;
+            xmlErrorText = $"""
+                Could not convert XML document to json. See exception message for details:
+                {ex.Message}
+                """;
+        }
     }
 
     private static void RemoveIrrelevantXmlNodes(XmlDocument xmlDocument)
@@ -39,25 +57,78 @@ public partial class DataViewer
             xmlDocument.RemoveChild(irrelevantNode);
     }
 
-    private async Task FilterAsync()
+    private async Task PreParse()
     {
-        parsing = true;
-        var jmesPath = new JmesPath();
-        jmesPath.FunctionRepository.Register("unique", new UniqueFunction());
         try
         {
-            await Task.Run(() => { queryResult = jmesPath.Transform(jsonText, query); });
+            jsonPreParseError = null;
+            preParsing = true;
+            jToken = await Task.Run(() => JToken.Parse(jsonText));
         }
         catch (Exception ex)
         {
-            queryResult = $"""
-                Could not parse query. See exception message for details:
+            jsonPreParseError = $"""
+                Could not parse json text. See exception message for details:
                 {ex.Message}
                 """;
+            jToken = null;
+        }
+        finally
+        {
+            preParsing = false;
+        }
+    }
+
+    private void CleanupPreParse()
+    {
+        jToken = null;
+    }
+
+    private async Task FilterAsync()
+    {
+        try
+        {
+            parsing = true;
+            var jmesPath = new JmesPath();
+            jmesPath.FunctionRepository.Register("unique", new UniqueFunction());
+            queryResult = jToken is null
+                ? await FilterFromText(jmesPath).ConfigureAwait(false)
+                : await FilterFromToken(jmesPath, jToken).ConfigureAwait(false);
         }
         finally
         {
             parsing = false;
+        }
+    }
+
+    private Task<string> FilterFromText(JmesPath jmesPath)
+    {
+        try
+        {
+            return Task.Run(() => jmesPath.Transform(jsonText, query));
+        }
+        catch (Exception ex)
+        {
+            return Task.FromResult($"""
+                Could not transform and filter query. See exception message for details:
+                {ex.Message}
+                """);
+        }
+    }
+
+    private async Task<string> FilterFromToken(JmesPath jmesPath, JToken jToken)
+    {
+        try
+        {
+            var transformResult = await Task.Run(async () => await jmesPath.TransformAsync(jToken, query));
+            return transformResult.ToString()!;
+        }
+        catch (Exception ex)
+        {
+            return $"""
+                Could not filter query. See exception message for details:
+                {ex.Message}
+                """;
         }
     }
 
