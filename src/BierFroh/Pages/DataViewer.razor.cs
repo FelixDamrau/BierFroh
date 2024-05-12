1233f56140f4af70e5fb1bced40e0dd2c89cc1c6
Felix Damrau <mail@felixdamrau.de>
2024-05-12 14:46:06 +0200

    Add basic data viewer & filter

---
+++ src/BierFroh/Pages/DataViewer.razor.cs
@@ -0,0 +1,82 @@
+﻿using System.Xml;
+using BierFroh.Model;
+using BierFroh.Modules.DataViewer;
+using DevLab.JmesPath;
+using Microsoft.AspNetCore.Components.Forms;
+using Newtonsoft.Json;
+
+namespace BierFroh.Pages;
+public partial class DataViewer
+{
+    private const long maxFileSize = 209_715_200; // 200MB
+    private string? xmlText;
+    private string? jsonText;
+    private string? query;
+    private string queryResult = string.Empty;
+    private string cleanResults = string.Empty;
+
+    private void Parse()
+    {
+        if (string.IsNullOrEmpty(xmlText))
+        {
+            jsonText = string.Empty;
+            return;
+        }
+        var xmlDocument = new XmlDocument();
+        xmlDocument.LoadXml(xmlText);
+        RemoveIrrelevantXmlNodes(xmlDocument);
+
+        jsonText = JsonConvert.SerializeXmlNode(xmlDocument, Newtonsoft.Json.Formatting.Indented, omitRootObject: true);
+    }
+
+    private static void RemoveIrrelevantXmlNodes(XmlDocument xmlDocument)
+    {
+        var nodes = xmlDocument.ChildNodes.OfType<XmlNode>().ToList();
+        foreach (var irrelevantNode in nodes.Where(n => n is XmlDocumentType or XmlDeclaration))
+            xmlDocument.RemoveChild(irrelevantNode);
+    }
+
+    private void Filter()
+    {
+        var jmesPath = new JmesPath();
+        jmesPath.FunctionRepository.Register("unique", new UniqueFunction());
+        try
+        {
+            queryResult = jmesPath.Transform(jsonText, query);
+        }
+        catch (Exception ex)
+        {
+            queryResult = $"""
+                Could not parse query. See exception message for details:
+                {ex.Message}
+                """;
+        }
+    }
+
+    private void Extract()
+    {
+        try
+        {
+            var data = JsonConvert.DeserializeObject<List<string>>(queryResult) ?? [];
+            cleanResults = string.Join(Environment.NewLine, data);
+        }
+        catch (Exception ex)
+        {
+            cleanResults = $"""
+                Could not extract data. See exception message for details:
+                {ex.Message}
+                """;
+        }
+    }
+
+    private async Task UploadXml(InputFileChangeEventArgs e)
+    {
+        xmlText = await BrowserFileReader.ReadFile(e.File, maxFileSize);
+    }
+
+    private async Task UploadJson(InputFileChangeEventArgs e)
+    {
+        xmlText = string.Empty;
+        jsonText = await BrowserFileReader.ReadFile(e.File, maxFileSize);
+    }
+}
---
+++ src/Modules/DataViewer/UniqueFunction.cs
@@ -0,0 +1,35 @@
+﻿using DevLab.JmesPath.Functions;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace BierFroh.Modules.DataViewer;
+
+public class UniqueFunction : JmesPathFunction
+{
+    public UniqueFunction() : base("Unique", 1)
+    {
+    }
+
+    public override JToken Execute(params JmesPathFunctionArgument[] args)
+    {
+        var jArray = (JArray)args[0].Token;
+        var list = JsonConvert
+            .DeserializeObject<List<object>>(jArray.ToString())!
+            .ToHashSet();
+        return new JArray(list);
+    }
+
+    public override void Validate(params JmesPathFunctionArgument[] args)
+    {
+        base.Validate(args);
+        var token = args[0].Token;
+        if (token is not JArray)
+        {
+            var message = $"""
+                The custom unique function expects a single array.
+                The provided type was '{token.Type}'.
+                """;
+            throw new InvalidOperationException(message);
+        }
+    }
+}
